402edb9fd780008f993f90fa3efe9f173c251ba7
Miguel Lopes <132863461+miiglops@users.noreply.github.com>
2023-09-17 23:22:05 -0300

    Add files via upload

--- Calculadora.java
+++ Calculadora.java
@@ -8,31 +8,26 @@ public class Calculadora {
         float n1 = (float) keyboard.nextFloat(); // nextFloat usado para float
         
         System.out.print("Digite o sinal da operação que você deseja fazer(+, - , x , /): ");
-        String operacaoString = keyboard.next();
+        String operacaoString = keyboard.next(); // next e nextLine usado para String
 
         System.out.print("Digite mais um número: ");
         float n2 = (float) keyboard.nextFloat();
 
+        //.equals é usado para comparar o conteúdo de uma string ao invés de ==
         if (operacaoString.equals("+")) {
             float n3 = (float) n1 + n2;
             System.out.println("A soma é: "+n3);
-        } else{
-            if (operacaoString.equals("-")) {
-                float n3 = (float) n1 - n2;
-                System.out.println("A subtração é: "+n3);
-            } else {
-                if (operacaoString.equals("x")) {
-                    float n3 = (float) n1 * n2;
-                    System.out.println("A multiplicação é: "+n3);
-                } else {
-                    if (operacaoString.equals("/")) {
-                        float n3 = (float) n1 / n2;
-                        System.out.println("A divisão é: "+n3);
-                    } else {
-                        System.out.println("Operação não reconhecida. Tente novamente!");
-                    }
-                }
-            }
+        } else if (operacaoString.equals("-")) {
+            float n3 = (float) n1 - n2;
+            System.out.println("A subtração é: "+n3);
+        } else if (operacaoString.equals("x")) {
+            float n3 = (float) n1 * n2;
+            System.out.println("A multiplicação é: "+n3);
+        } else if (operacaoString.equals("/")) {
+            float n3 = (float) n1 / n2;
+            System.out.println("A divisão é: "+n3);
+        } else {
+            System.out.println("Operação não reconhecida. Tente novamente!");
         }
     }    
 }
